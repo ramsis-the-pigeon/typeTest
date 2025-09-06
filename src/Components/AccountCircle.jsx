336415ef77b3c800d4dafaa389330466973a210a
Youcef <yoossfidiot@gmail.com>
2025-09-06 21:56:12 +0200

    Add MUI icons and modal functionality to AccountCircle component

--- src/Components/AccountCircle.jsx
+++ src/Components/AccountCircle.jsx
@@ -1,9 +1,29 @@
 import React from "react";
-
+import { useState } from "react";
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Modal } from "@mui/material";
 const AccountCircle = () => {
+
+    const [open, setOpen] = useState(false);
+    const handleModalOpen = () => setOpen(true);
+    const handleModalClose = () => setOpen(false);
+
+
     return (
         <div className="account-circle">
-            AccountCircle
+            <AccountCircleIcon onClick={handleModalOpen}/>
+            <Modal
+                open={open}
+                onClose={handleModalClose}
+                style={{
+                    display: 'flex', 
+                    alignItems: 'center', 
+                    justifyContent: 'center',
+                    padding: '40px',
+                }}
+            >
+                <div>modal</div>
+            </Modal>
         </div>
 
     )}
